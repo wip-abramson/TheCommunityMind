6035600fb3a46930f1401d63e3dc05b6c1a02e7f
wip-abramson <wip.abramson@gmail.com>
2017-03-13 18:16:12 +0000

    Refactored question input to text input which question input uses

--- app/components/QuestionInput.js
+++ app/components/QuestionInput.js
@@ -1,43 +1,24 @@
 import React, { PropTypes } from 'react'
+import TextInput from './TextInput'
 
 function QuestionInput (props) {
   let input;
 
   return (
+
     <div className="col-sm-12">
-        <form onSubmit={function(e) {
-          e.preventDefault()
-          // console.log("Props",props)
-          // if (props.question.trim()) {
-          //   return;
-          // }
-          props.onAskQuestion(input.value)
-          input.value="";
-        }}>
-          <div className="form-group">
-            <input
-              ref={node => {
-                input = node
-              }}
-              className="form-control"
-              placeholder={props.placeholder}
-              type="text" />
-          </div>
-          <div className="form-group col-sm-4 col-sm-offset-4">
-            <button
-              className="btn btn-block btn-success"
-              type="submit">
-                Ask
-              </button>
-          </div>
-        </form>
-      </div>
+      <TextInput
+        placeholder={props.placeholder}
+        submitName="Ask"
+        onSubmit={props.onAskQuestion}
+      />
+
+    </div>
   )
 }
 
 QuestionInput.PropTypes = {
-  // question: PropTypes.string.isRequired,
-  // onUpdateQuestion: PropTypes.func,
+
   placeholder: PropTypes.string.isRequired,
   onAskQuestion: PropTypes.func.isRequired
 }
---
+++ app/components/TextInput.js
@@ -0,0 +1,43 @@
+import React, { PropTypes } from 'react'
+
+function TextInput (props) {
+  let input;
+
+  return (
+        <form onSubmit={function(e) {
+          e.preventDefault()
+
+          if (! input.value.trim()) {
+            return;
+          }
+          props.onSubmit(input.value)
+          input.value="";
+        }}>
+          <div className="form-group">
+            <input
+              ref={node => {
+                input = node
+              }}
+              className="form-control"
+              placeholder={props.placeholder}
+              type="text" />
+          </div>
+          <div className="form-group col-sm-4 col-sm-offset-4">
+            <button
+              className="btn btn-block btn-success"
+              type="submit">
+                {props.submitName}
+              </button>
+          </div>
+        </form>
+
+  )
+}
+
+TextInput.PropTypes = {
+  submitName: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
+  onSubmit: PropTypes.func.isRequired
+}
+
+module.exports = TextInput;

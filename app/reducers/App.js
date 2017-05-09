ea806660eb15272452442f0fb50dfe6cb75f1a1f
wip-a <wip.abramson@gmail.com>
2017-05-09 17:46:43 +0100

    Changed to cookie auth

--- app/reducers/App.js
+++ app/reducers/App.js
@@ -5,23 +5,44 @@ import {ApolloClient, createNetworkInterface} from "react-apollo";
 
 const networkInterface = createNetworkInterface({
   uri: 'http://0.0.0.0:5000/graphql',
-  // opts: {
-  //     credentials: 'same-origin',
-  // },
+  opts: {
+      credentials: 'include',
+  },
 })
 
-networkInterface.use([{
-  applyMiddleware(req, next) {
-    if (!req.options.headers) {
-      req.options.headers = {};  // Create the header object if needed.
-    }
+// networkInterface.use([{
+//   applyMiddleware(req, next) {
+//
+//     if (!req.options.headers) {
+//       req.options.headers = {};  // Create the header object if needed.
+//     }
+//     req.options.headers = {
+//       authourization: localStorage.getItem('token') ? localStorage.getItem('token') : null
+//    }
+//     next();
+//   }
+// }]);
 
-    // get the authentication token from local storage if it exists
-    const token = localStorage.getItem('token');
-    req.options.headers.authorization = token ? `Bearer ${token}` : null;
-    next();
-  }
-}]);
+// export const getClientError = errors => {
+//   if (!errors) return;
+//   const error = errors[0].message;
+//   return (error.indexOf('{"_error"') === -1) ? {_error: 'Server query error'} : JSON.parse(error);
+// }
+//
+// export const fetchGraphQL = async (graphParams) => {
+//   const authToken = localStorage.getItem('myToken');
+//   const res = await fetch('http://0.0.0.0:5000/graphql', {
+//     method: 'post',
+//     headers: {
+//       'Content-Type': 'application/json',
+//       'Authorization': `Bearer ${authToken}`
+//     },
+//     body: JSON.stringify(graphParams)
+//   });
+//   const resJSON = await res.json();
+//   const {data, errors} = resJSON;
+//   return {data, error: getClientError(errors)}
+// }
 
 // Finally, create your ApolloClient instance with the modified network interface
 export const client = new ApolloClient({
--- server/server.js
+++ server/server.js
@@ -3,36 +3,45 @@ import {graphiqlExpress} from "graphql-server-express";
 import {schema} from "./src/schema";
 import cors from "cors";
 import graphqlHTTP from "express-graphql";
-
+import session from 'express-session';
 
 const PORT = 5000
 const server = express();
 
-server.use('*', cors({origin: 'http://0.0.0.0:8080'}))
+server.use('*', cors({origin: 'http://0.0.0.0:8080', credentials: true}))
 
 server.use(express.static('../public'))
 
-// server.use('/graphql', jwt({
-//   secret: 'shhhhhhared-secret',
-//   requestProperty: 'auth',
-//   credentialsRequired: false,
-// }));
-server.use('/graphql', function (req, res, done) {
-  // const user = User.get(req.auth.sub);
-  req.context = {
-    // user: user,
-  }
-  done();
-});
-server.use('/graphql', graphqlHTTP(req => ({
-    schema: schema,
-    context: req.context,
-  })
-));
+server.use('/graphql', session({
+  secret: 'shhhhhhared-secret',
+  cookie: { maxAge: 60000 },
+  resave: true,
+  saveUninitialized: true
+}), );
+// httpGraphQLHandler
+// const httpGraphQLHandler = async (req, res) => {
+//   const {query, variables, ...rootVals} = req.body;
+//   const authToken = req.user || {};
+//   const result = await graphql(schema, query, {authToken, ...rootVals}, variables);
+//   res.send(result);
+// }
+// server.use('/graphql', function (req, res, done) {
+//   var authToken = req.options.headers.authorization
+//   const user = User.get(authToken);
+//   req.context = {
+//     user: user,
+//   }
+//   done();
+// });
+
+server.use('/graphql', graphqlHTTP((request) => ({
+  schema: schema,
+  rootValue: { session: request.session },
+})));
 
 server.use('/graphiql', graphiqlExpress({
   endpointURL: '/graphql'
 }));
 
 
-server.listen(PORT, () => console.log(`GraphQL Server is now running on http://localhost:${PORT}`));
+server.listen(PORT, () => console.log(`GraphQL Server is now running on http://0.0.0.0:${PORT}`));

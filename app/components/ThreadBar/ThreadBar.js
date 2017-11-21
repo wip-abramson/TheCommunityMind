e724d15d27d5bc2e20baa35336d73aba9a11ac74
wip-a <wip.abramson@gmail.com>
2017-11-21 07:24:35 +0000

    Thread Bar populated from database

--- app/components/ThreadBar/ThreadBar.js
+++ app/components/ThreadBar/ThreadBar.js
@@ -4,10 +4,20 @@
 import React from 'react';
 
 const ThreadBar = (props) => {
+  if (props.loading) {
+    return <p>Loading ...</p>;
+  }
+  if (props.error) {
+    return <p>{props.error.message}</p>;
+  }
+  // console.log(props.tags.length);
   return (
     <div style={{padding: "10px"}}>
       <button>Home</button>
-      <button>Creativity</button>
+      {props.tags.map(tag => {
+        return <button>{tag.name[0].toUpperCase() + tag.name.slice(1)}</button>
+      })}
+
       <button>Popular in the Community</button>
       <button>More ...</button>
     </div>
--- app/components/ThreadBar/ThreadBarContainer.js
+++ app/components/ThreadBar/ThreadBarContainer.js
@@ -2,8 +2,12 @@
  * Created by will on 21/11/17.
  */
 import React from 'react'
+import { graphql, compose } from 'react-apollo';
 
 import ThreadBar from './ThreadBar';
+
+import TOP_TAGS_QUERY from '../../graphql/querys/topTags.query';
+
 
 
 
@@ -16,4 +20,12 @@ class ThreadBarContainer extends React.Component {
   }
 }
 
-export default ThreadBarContainer;
+export default compose(
+  graphql(TOP_TAGS_QUERY, {
+    props: ({ ownProps, data: { loading, error, topTags } }) => ({
+      loading,
+      error,
+      tags: topTags
+    })
+  })
+)(ThreadBar);

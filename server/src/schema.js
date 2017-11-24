f3ff2b0f92ad807a2e8291c6ad57c73d83f390f0
wip-a <wip.abramson@gmail.com>
2017-11-24 03:50:34 +0000

    Added Connections and Edges to Why, WhatIf, How and Question

--- server/src/schema.js
+++ server/src/schema.js
@@ -11,167 +11,8 @@
 //   GraphQLList
 // } from 'graphql';
 import {resolvers} from "./resolvers";
-// import {resolver, relay} from 'graphql-sequelize';
 import { makeExecutableSchema } from 'graphql-tools'
 
-// import { Conn, Tag, User, Why, WhatIf, How, Question } from './db';
-//
-// const { sequelizeConnection, sequelizeNodeInterface } = relay;
-// const {
-//   nodeInterface,
-//   nodeField,
-//   nodeTypeMapper
-// } = sequelizeNodeInterface(Conn);
-
-// const Date = new GraphQLScalarType();
-
-// const tagType = new GraphQLObjectType({
-//   name: Tag.name,
-//   fields: {
-//     id: {
-//       type: GraphQLID
-//     },
-//     name: {
-//       type: GraphQLString
-//     },
-//     followers: {
-//       type: () => new GraphQLList(userType)
-//     },
-//     interfaces: [nodeInterface]
-//   }
-// })
-//
-//
-//
-// const userType =  new GraphQLObjectType({
-//   name: User.name,
-//   fields: {
-//     id: {
-//       type: GraphQLID
-//     },
-//     username: {
-//       type: GraphQLString,
-//       required: true
-//     },
-//     password: {
-//       type: GraphQLString
-//     },
-//     interestedIn: {
-//       type: () => new GraphQLList(tagType)
-//     }
-//   }
-// });
-
-// const userTaskConnection = sequelizeConnection({
-//   name: 'userTask',
-//   nodeType: tagType,
-//   target: User.Tasks | Task, // Can be an association for parent related connections or a model for "anonymous" connections
-//   // if no orderBy is specified the model primary key will be used.
-//   orderBy: new GraphQLEnumType({
-//     name: 'UserTaskOrderBy',
-//     values: {
-//       AGE: {value: ['createdAt', 'DESC']}, // The first ENUM value will be the default order. The direction will be used for `first`, will automatically be inversed for `last` lookups.
-//       TITLE: {value:  ['title', 'ASC']},
-//       CUSTOM: {value:  [function (source, args, context, info) {}, 'ASC']} // build and return custom order for sequelize orderBy option
-//     }
-//   }),
-//   where: function (key, value, currentWhere) {
-//     // for custom args other than connectionArgs return a sequelize where parameter
-//
-//     return {[key]: value};
-//   },
-//   connectionFields: {
-//     total: {
-//       type: GraphQLInt,
-//       resolve: ({source}) => {
-//         /*
-//          * We return a object containing the source, edges and more as the connection result
-//          * You there for need to extract source from the usual source argument
-//          */
-//         return source.countTasks();
-//       }
-//     }
-//   },
-//   edgeFields: {
-//     wasCreatedByUser: {
-//       type: GraphQLBoolean,
-//       resolve: (edge) => {
-//         /*
-//          * We attach the connection source to edges
-//          */
-//         return edge.node.createdBy === edge.source.id;
-//       }
-//     }
-//   }
-// });
-
-// const userTagConnection = sequelizeConnection({
-//   name: "userTag",
-//   nodeType: tagType,
-//   target: User.Tags || Tag,
-//
-//   orderBy: new GraphQLEnumType({
-//     name: 'UserTaskOrderBy',
-//     values: {
-//       AGE: {value: ['createdAt', 'DESC']}, // The first ENUM value will be the default order. The direction will be used for `first`, will automatically be inversed for `last` lookups.
-//       TITLE: {value:  ['title', 'ASC']},
-//       CUSTOM: {value:  [function (source, args, context, info) {}, 'ASC']} // build and return custom order for sequelize orderBy option
-//     }
-//   }),
-//   where: function (key, value, currentWhere) {
-//     // for custom args other than connectionArgs return a sequelize where parameter
-//
-//     return {[key]: value};
-//   },
-//   connectionFields: {
-//     total: {
-//       type: GraphQLInt,
-//       resolve: ({source}) => {
-//         /*
-//          * We return a object containing the source, edges and more as the connection result
-//          * You there for need to extract source from the usual source argument
-//          */
-//         return source.countTasks();
-//       }
-//     }
-//   },
-//   edgeFields: {
-//     wasCreatedByUser: {
-//       type: GraphQLBoolean,
-//       resolve: (edge) => {
-//         /*
-//          * We attach the connection source to edges
-//          */
-//         return edge.node.createdBy === edge.source.id;
-//       }
-//     }
-//   }
-//
-// })
-//
-// nodeTypeMapper.mapTypes({
-//   [User.name]: userType,
-//   [Tag.name]: tagType,
-//
-// });
-//
-// const testSchema = new GraphQLSchema({
-//   query: new GraphQLObjectType({
-//     name: 'RootType',
-//     fields: {
-//       user: {
-//         type: userType,
-//         args: {
-//           id: {
-//             type: GraphQLID
-//           }
-//         },
-//         resolve: resolver(User)
-//       },
-//       node: nodeField
-//     }
-//   })
-// });
 
 const typeDefs = `
   scalar Date
@@ -240,32 +81,35 @@ const typeDefs = `
     id: ID!
     question: Question!
     whatIfs: [WhatIf]!
-    whatIfFeed(cursor: String): WhatIfFeed
     createdAt: Date!
   }
   
-  type WhyFeed {
-    # Specifies timestamp in list of whys to populate from
-    cursor: String!
-    
-    # list of returned whys
-    whys: [Why]!
+  type WhatIfConnection {
+    edges: [WhatIfEdge]
+    pageInfo: PageInfo!
   }
   
+  type WhatIfEdge {
+    node: WhatIf!
+    cursor: String!
+  }
+
   type WhatIf implements QuestionType{
     id: ID!
     question: Question!
     createdAt: Date!
     hows: [How]!
-    howFeed(cursor: String): HowFeed
   }
   
-  type WhatIfFeed {
-    # Specifies timestamp in list of hows to populate from
+  
+  type HowConnection {
+    edges: [HowEdge]
+    pageInfo: PageInfo!
+  }
+  
+  type HowEdge {
+    node: How!
     cursor: String!
-    
-    # list of returned hows
-    whatIfs: [WhatIf]!
   }
   
   type How implements QuestionType{
@@ -274,13 +118,6 @@ const typeDefs = `
     createdAt: Date!
   }
   
-  type HowFeed {
-    # Specifies timestamp in list of hows to populate from
-    cursor: String!
-    
-    # list of returned hows
-    hows: [How]!
-  }
   
   type User {
     id: ID!
@@ -290,9 +127,9 @@ const typeDefs = `
     whys: [Why]
     hows: [How]
     whatIfs: [WhatIf]
-    questions: [QuestionType]
+    questions(first: Int, after: String, last: Int, before: String): QuestionTypeConnection
     jwt: String # json web token for access
-    staredQuestions: [QuestionType]
+    staredQuestions(first: Int, after: String, last: Int, before: String): QuestionTypeConnection
     followersCount: Int!
     followers: [User]
     followsCount: Int!
@@ -314,12 +151,9 @@ const typeDefs = `
    tags: [Tag]    
    whys(first: Int, after: String, last: Int, before: String): WhyConnection!
    user(id: ID!): User
-   whatIfs(whyId: ID!): [WhatIf]
-   hows(whatIfId: ID!): [How]
-   whyFeed(cursor: String): WhyFeed
-   howFeed(whatIfId: ID!, cursor: String): HowFeed
-   whatIfFeed(whyId: ID!, cursor: String): WhatIfFeed
-  }
+   whatIfs(whyId: ID!, first: Int, after: String, last: Int, before: String): WhatIfConnection!
+   hows(whatIfId: ID!, first: Int, after: String, last: Int, before: String): HowConnection!
+   }
   
   type Mutation {
     createWhy(question: String!, tagIds: [ID]!): Why

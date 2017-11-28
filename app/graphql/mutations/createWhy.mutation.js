3a5483e994110a1a8261a842db80b757690f34e6
wip-a <wip.abramson@gmail.com>
2017-11-28 05:33:53 +0000

    minor refactor

--- app/graphql/mutations/createWhy.mutation.js
+++ app/graphql/mutations/createWhy.mutation.js
@@ -7,10 +7,11 @@ import QUESTION_FRAGMENT from '../fragments/question.fragment';
 const CREATE_WHY_MUTATION = gql`
   mutation createWhy($question: String!, $tagIds: [ID]!) {
     createWhy(question: $question, tagIds: $tagIds) {
-     id
-     question {
-      ... QuestionFragment
-     }
+       id
+       question {
+        ... QuestionFragment
+       }
+       
     }
     
   }

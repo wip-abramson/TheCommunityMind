baf9e443124f04772d631d663d2d109fafac6429
wip-a <wip.abramson@gmail.com>
2017-12-19 16:03:46 +0000

    Added modal content allowing creation of questions in backend for Why Whatif and How

--- app/components/AskQuestionPopup/AskQuestionPopup.js
+++ app/components/AskQuestionPopup/AskQuestionPopup.js
@@ -6,17 +6,36 @@ import FaClose from 'react-icons/fa/close'
 
 import AskWhy from './AskWhy';
 import AskWhatIf from './AskWhatIf';
+import AskHow from './AskHow';
+
+import { WHY, WHATIF, HOW } from '../../actions/AskQuestionPopup';
 
 import styles from './askQuestionPopup.css';
 
 const AskQuestionPopup = (props) => {
+  console.log(props.askQuestionPopup)
+  function getModalContent() {
+
+    if (props.askQuestionPopup.questionType === HOW) {
+      return <AskHow placeholder="How ...?" hideAskQuestionPopup={props.hideAskQuestionPopup} questionType={props.askQuestionPopup.questionType}/>
+    }
+    if (props.askQuestionPopup.questionType === WHATIF) {
+      return <AskWhatIf placeholder="What If ...?" hideAskQuestionPopup={props.hideAskQuestionPopup} questionType={props.askQuestionPopup.questionType}/>
+    }
+    return <AskWhy placeholder="Why ...?" hideAskQuestionPopup={props.hideAskQuestionPopup} questionType={props.askQuestionPopup.questionType} />
+  }
 
   return (
-    <div className={styles.modal}>
+    <div className={props.askQuestionPopup.visible ? styles.modalVisible : styles.modal}>
 
-      <div className={styles.modalContent}><FaClose className={styles.close}/><AskWhatIf/></div>
+      <div className={styles.modalContent}>
+        <FaClose onClick={props.hideAskQuestionPopup} className={styles.close}/>
+        {getModalContent()}
+      </div>
     </div>
   )
 };
+
+
 
 export default AskQuestionPopup;

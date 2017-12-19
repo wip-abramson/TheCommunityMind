4272ed9227d8f1f3e98dc69896e2c95d3b7be2ec
wip-a <wip.abramson@gmail.com>
2017-12-19 16:01:59 +0000

    Added questionType method to askQuestionPopup in redux

--- app/actions/AskQuestionPopup.js
+++ app/actions/AskQuestionPopup.js
@@ -3,11 +3,21 @@
  */
 export const SHOW_POPUP = "SHOW_POPUP";
 export const HIDE_POPUP = "HIDE_POPUP";
+export const SET_QUESTION_TYPE = "SET_QUESTION_TYPE";
+
+export const WHY = "WHY";
+export const WHATIF = "WHATIF";
+export const HOW = "HOW";
 
 export function showAskQuestionPopup(currentWhy, currentWhatIf) {
-  return {type: SHOW_POPUP, currentWhy, currentWhatIf}
+  console.log("SHOW ASK Q POPUP")
+  return { type: SHOW_POPUP, currentWhy: currentWhy, currentWhatIf: currentWhatIf }
 }
 
 export function hideAskQuestionPopup() {
-  return {type: HIDE_POPUP}
+  return { type: HIDE_POPUP }
+}
+
+export function setQuestionType(questionType) {
+  return { type: SET_QUESTION_TYPE, questionType: questionType}
 }
--- app/reducers/AskQuestionPopup.js
+++ app/reducers/AskQuestionPopup.js
@@ -1,15 +1,22 @@
 /**
  * Created by will on 19/12/17.
  */
-import { SHOW_POPUP, HIDE_POPUP } from '../actions/AskQuestionPopup';
+import { SHOW_POPUP, HIDE_POPUP, SET_QUESTION_TYPE } from '../actions/AskQuestionPopup';
 
 export function askQuestionPopup(state = {}, action) {
   switch (action.type) {
     case SHOW_POPUP:
-      return Object.assign({}, state, {visible: true, currentWhy: action.currentWhy, currentWhatIf: action.currentWhatIf});
+      return Object.assign({}, state, {
+        visible: true,
+        currentWhy: action.currentWhy,
+        currentWhatIf: action.currentWhatIf
+      });
     case HIDE_POPUP:
-      return Object.assign({}, state, {currentWhy: null, currentWhatIf: null, visible: false});
+      return Object.assign({}, state, { currentWhy: null, currentWhatIf: null, visible: false });
+    case SET_QUESTION_TYPE:
+      return Object.assign({}, state, { questionType: action.questionType });
     default:
+      console.log("DEFAULT")
       return state;
   }
 }

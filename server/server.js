b2c544be970adfb96a95747f6de08ffbb4084adc
wip-a <wip.abramson@gmail.com>
2017-12-27 11:52:27 +0000

    Changed server so cors works with nginx

--- server/server.js
+++ server/server.js
@@ -33,6 +33,19 @@ server.use('/graphql', bodyParser.json(), jwt({
   },
 })));
 
+server.use("/graphql", function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Content-Length, X-Requested-With"
+  );
+  if (req.method === "OPTIONS") {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
 
 
 server.use('/graphiql', graphiqlExpress({

35235ca6e8e87c4127a56942999e7e042c4f54ab
wip-a <wip.abramson@gmail.com>
2018-01-14 15:55:25 +0000

    no autorestart for pm2

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
     "script"      : "./server.js",
     "watch"       : false,
     "error_file"      : "err.log",
-    "autorestart": false,
+    "no-autorestart": true,
     "ignore_watch" : ["node_modules", "./mind.sqlite"],
     "exec_interpreter" : "babel-node",
     "exec_mode"        : "fork"
